e1d2c9ae0fb62efa9d3a6d69e959501efb818608
drogi17 <38162697+drogi17@users.noreply.github.com>
2020-03-16 15:09:06 +0200

    Add files via upload

--- pyAnywhere/pyAnywhere.py
+++ pyAnywhere/pyAnywhere.py
@@ -1,9 +1,20 @@
 import requests, json
 
-class Error_pyAnywhere(Exception):
+class ErrorOutput(Exception):
     def __init__(self, text):
         self.txt = text
 
+def checking_server_response(response):
+    if response.status_code < 200:    # 1xx
+        return (True, 'info')
+    elif response.status_code <= 300: # 2xx
+        return (True, 'successfully')
+    elif response.status_code <= 400: # 3xx
+        return (True, 'redirection')
+    elif response.status_code <= 500: # 4xx
+        return (False, 'client_error')
+    elif response.status_code <= 600: # 5xx
+        return (False, 'server_error')
 
 class ConsoleSession(object):
     def __init__(self, username, session, console_id):
@@ -17,9 +28,10 @@ class ConsoleSession(object):
                 username=self.username, 
                 console_id=self.console_id
                 )
-            ).content.decode()
-        consoles_about = json.loads(response)
-        return consoles_about
+            )
+        if checking_server_response(response)[0]:
+            consoles_about = response.json()
+            return consoles_about
     def get_latest_output(self):
         if self.console_id:
             response = self.session.get(
@@ -27,9 +39,10 @@ class ConsoleSession(object):
                     username=self.username, 
                     console_id=self.console_id
                     )
-                ).content.decode()
-            consoles_about = json.loads(response)
-            return consoles_about
+                )
+            if checking_server_response(response)[0]:
+                consoles_about = response.json()
+                return consoles_about
         else:
             return None
     def send_input(self, command):
@@ -40,7 +53,8 @@ class ConsoleSession(object):
                     ),
                 json={'input': str(command)}
             )
-            return send_status.text
+            if checking_server_response(send_status)[0]:
+                return send_status.text
         else:
             return None
         
@@ -49,15 +63,15 @@ class Consoles(object):
     def __init__(self, username, session):
         self.session  = session
         self.username = username
-       
     def all(self):
         response = self.session.get(
             'https://www.pythonanywhere.com/api/v0/user/{username}/consoles/'.format(
                 username=self.username
                 )
-        ).content.decode()
-        all_consoles = json.loads(response)
-        return all_consoles
+        )
+        if checking_server_response(response)[0]:
+            all_consoles = response.json()
+            return all_consoles
     def console(self, console_id=None): 
         if console_id:
             return ConsoleSession(self.username, self.session, console_id)
@@ -68,11 +82,10 @@ class pyAnywhere(object):
             'https://www.pythonanywhere.com/api/v0/user/{username}/cpu/'.format(username=username),
             headers={'Authorization': 'Token {token}'.format(token=token)})
         if response.status_code == 200:
-            # self.token    = token
             self.username = username
             self.session  = requests.Session()
             self.session.headers.update({'Authorization': 'Token {token}'.format(token=token)})
         else:
-            raise Error_pyAnywhere("Incorrect data")
+            raise ErrorOutput("Incorrect data")
     def consoles(self):
         return Consoles(self.username, self.session)
